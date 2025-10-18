d632dfbdecc23e7dcf650a3f0a2e7b2a5258149e
3laa-boss <3laa.alawneh@gmail.com>
2025-10-18 16:07:20 +0300

    initalTests

--- src/main/java/edu/software/lms/Main.java
+++ src/main/java/edu/software/lms/Main.java
@@ -1,8 +1,15 @@
 package edu.software.lms;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello World!");
+
+        System.out.println("Enter username and password:");
+        Scanner sc = new Scanner(System.in);
+        String username = sc.nextLine();
+        String password = sc.nextLine();
+
 
     }
 }
---
+++ src/test/java/edu/software/lms/MainTest.java
@@ -0,0 +1,30 @@
+package edu.software.lms;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+
+    }
+    @AfterAll
+    static void afterAll() {
+
+    }
+
+    @Test
+    void testInitial() {
+        assertTrue(true);
+    }
+}
